570b3cf2477c78689d64e09a5e1f1043975cb840
Shrinidhi7 <shrinidhi7.ss@gmail.com>
2020-05-20 23:01:52 +0530

    Input from CSV and Text files

---
+++ inputdata.R
@@ -0,0 +1,15 @@
+? iris
+str(iris)
+str(iris3)
+iris
+iris3
+data("iris")
+X=scan()
+J<-read.table("C:/Users/lenovo/Documents/DATA/data1.txt",header=TRUE,sep="\t")
+
+str(J)
+j
+J
+P<-read.table("C:/Users/lenovo/Documents/DATA/original.txt",header=TRUE,sep="\t")
+q<-read.csv("C:/Users/lenovo/Documents/DATA/original.xls",header=TRUE)
+View(q)
