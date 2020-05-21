38a1604acdb7379d1afc08769990329dd3befeb7
Shrinidhi7 <shrinidhi7.ss@gmail.com>
2020-05-21 19:19:22 +0530

    updated & data preprocessed

--- House_price.R
+++ House_price.R
@@ -18,3 +18,24 @@ summary(df$n_hot_rooms)
 lv=0.3*quantile(df$rainfall,0.01)
 df$rainfall[df$rainfall<lv]<-lv
 summary(df$rainfall)
+j=mean(df$n_hos_beds,na.rm = TRUE)
+which(is.na(df$n_hos_beds))
+df$n_hos_beds[is.na(df$n_hos_beds)]<-mean(df$n_hos_beds,na.rm=TRUE)
+summary(df$n_hos_beds)
+which(is.na(df$n_hos_beds))
+pairs(~price+crime_rate,data=df)
+plot(df$price,df$crime_rate)
+df$crime_rate=log(1+df$crime_rate)
+df$avg_dist=(df$dist1+df$dist2+df$dist3+df$dist4)/4
+df2<-df[,-7:-10,]
+df<-df2
+rm(df2)
+df<-df[,-14]
+install.packages("dummies")
+df<-dummy.data.frame(df)
+df<-df[,-9]
+df<-df[,-14]
+cor(df)
+round(cor(df),2)
+#PARKS AND AIR QUALITY ARE HIGHLY CO_RELATED and henceone of them is removed to avoid multi-colinearity
+df<-df[,-16]
